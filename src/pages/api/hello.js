a6b4b8a156cef7dd14e54c9d0fcec0e1c3d0f700
William Mallett <mallett002@gmail.com>
2023-06-10 07:44:10 -0500

    server session

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "./auth/[...nextauth]"
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  const session = await getServerSession(req, res, authOptions);
+
+  // use session.accessToken to make api calls
+
+  res.status(200).json(session);
 }
