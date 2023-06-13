100cee2f49be2cd8a356157cba6e9f8350d1a4d0
William Mallett <mallett002@gmail.com>
2023-06-13 06:15:20 -0500

    use stuff from the token

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -3,8 +3,10 @@ import CognitoProvider from "next-auth/providers/cognito";
 
 // Get username off ID token?
 // Guide: https://kelvinmwinuka.com/social-login-with-cognito-and-nextauth
-
 // guide: https://codevoweb.com/setup-and-use-nextauth-in-nextjs-13-app-directory/
+
+// Sub: f4a804d8-d081-709d-d6ab-14448fc35e23
+// userId: Google_101398132283206901435
 export const authOptions = {
     providers: [CognitoProvider({
         clientId: process.env.COGNITO_CLIENT_ID,
@@ -15,21 +17,38 @@ export const authOptions = {
     })],
 
     callbacks: {
+        // user, account, profile and isNewUser (first time log in), other calls, just token
         async jwt(jwtArg) {
-            console.log({jwtArg});
-            
-            if (jwtArg.account) {
-                jwtArg.token.accessToken = jwtArg.account.access_token
+            console.log(JSON.stringify({ jwtArg }, null, 2));
+            const {account, token, profile} = jwtArg;
+
+            // put things on the token to persist them to the session
+            const sessionToken = token;
+
+            if (account) {
+                sessionToken.accessToken = account.access_token;
+                sessionToken.idToken = account.id_token;
             }
 
-            return jwtArg.token;
+            if (profile) {
+                sessionToken.givenName = profile.given_name;
+            }
+
+            return sessionToken;
         },
-        async session(sessionArg) {
-            console.log(JSON.stringify({sessionArg}, null, 2));
-            
-            const { session, token, user } = sessionArg;
-            
-            session.accessToken = token.accessToken
+
+        // getSession(), useSession(), /api/auth/session
+        async session(sessionArg) { // session, token
+            // The session callback is called whenever a session is checked
+            console.log(JSON.stringify({ sessionArg }, null, 2));
+
+            // put things on the session to expose them to the app
+
+            const { session, token } = sessionArg;
+
+            session.accessToken = token.accessToken;
+            session.idToken = token.idToken;
+            session.givenName = token.givenName;
 
             return session;
         }
--- src/pages/protected.js
+++ src/pages/protected.js
@@ -7,19 +7,23 @@ import Image from 'next/image'
 const Protected = () => {
     const { data } = useSession();
 
+    console.log(({ dataInsUseSession: data }));
+
     if (data && data.user) {
-        console.log(data.user);
 
         return (
-            <div style={{paddingLeft: 20, marginTop: 30}}>
-                <h3 style={{marginBottom: 20}}>User Info</h3>
-                <div style={{ display: 'flex', justifyContent: 'space-between', width: 200 }}>
-                    <Image
-                        src={data.user.image}
-                        width={30}
-                        height={30}
-                        alt="Picture of the author"
-                    />
+            <div style={{ paddingLeft: 20, marginTop: 30 }}>
+                <h3 style={{ marginBottom: 20 }}>User Info</h3>
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <div style={{paddingRight: 15}}>
+                        <Image
+                            src={data.user.image}
+                            width={30}
+                            height={30}
+                            alt="Picture of the author"
+                        />
+                    </div>
+                    <p style={{ paddingRight: 15 }}>{data.givenName}</p>
                     <p>{data.user.email}</p>
                 </div>
             </div>
