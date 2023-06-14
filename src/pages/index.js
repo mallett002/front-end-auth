d26b3d6e974968884b3749e500504fae16d2546f
William Mallett <mallett002@gmail.com>
2023-06-14 07:55:29 -0500

    get gift working

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,9 @@ import {
   LogoutButton,
   ProfileButton
 } from "../components/buttons";
-import { useSession } from "next-auth/react";
+import { useSession, getSession } from "next-auth/react";
 
-export default function Home({gift}) {
+export default function Home({ gift }) {
   const { data, status } = useSession();
 
   console.log({ data, gift });
@@ -14,7 +14,7 @@ export default function Home({gift}) {
   if (status === 'loading') {
     return <p>Loading...</p>
   }
-  
+
   if (status === 'unauthenticated') {
     return (
       <div>
@@ -41,10 +41,22 @@ export default function Home({gift}) {
 }
 
 // TODO: Maybe make this a server side component and use "useServerSession" to get the token
-export const getServerSideProps = async () => {
-  const res = await fetch(`${process.env.WISH_LIST_SERVER_DOMAIN}/repos/vercel/next.js`);
-  const gift = await res.json();
-  console.log({stuffInServer: gift});
+export const getServerSideProps = async (context) => {
+  const session = await getSession(context);
+  
 
-  return { props: { gift } };
-}
+  if (session) {
+    console.log({token: session.accessToken});
+    const res = await fetch(`${process.env.WISH_LIST_SERVER_DOMAIN}/families/family1/users/mallett002@gmail.com/gifts/gift1`, {
+      headers: {
+        Authorization: `Bearer ${session.idToken}`
+      }
+    });
+    const gift = await res.json();
+    console.log({ stuffInServer: gift });
+
+    return { props: { gift } };
+  }
+
+  return { props: {} };
+};
